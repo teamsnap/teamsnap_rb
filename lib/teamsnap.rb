13004eb337f7aa8abc5de979608c16b9ce62eaae
Shane Emmons <shane@emmons.io>
2015-02-19 15:21:27 +0000

    update irregular inflections

--- lib/teamsnap.rb
+++ lib/teamsnap.rb
@@ -19,9 +19,13 @@ Faraday::Request.register_middleware(
 )
 
 Inflecto.inflections do |inflect|
+  inflect.irregular "member_preferences", "member_preferences"
   inflect.irregular "member_preferences", "members_preferences"
+  inflect.irregular "opponent_results", "opponent_results"
   inflect.irregular "opponent_results", "opponents_results"
+  inflect.irregular "team_preferences", "team_preferences"
   inflect.irregular "team_preferences", "teams_preferences"
+  inflect.irregular "team_results", "team_results"
   inflect.irregular "team_results", "teams_results"
 end
 
